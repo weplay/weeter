9a4b28113ff983dca5d42d9e47458391c7575a0d
Noah Davis <noahd1@yahoo.com>
2010-11-04 18:15:34 -0400

    add more runner specs

--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -8,12 +8,44 @@ describe Weeter::Runner do
       EM.should_receive(:run).twice.and_yield
       EM.stub!(:start_server)
 
-      http = mock(:http, :callback => nil)
-      http_request = mock(:http_request, :get => http)
-      EM::HttpRequest.stub!(:new => http_request)
+      @http = mock(:http, :callback => nil)
+      @http_request = mock(:http_request, :get => @http)
+      EM::HttpRequest.stub!(:new => @http_request)
 
       Weeter::Configuration.instance.oauth = nil
       Weeter::Configuration.instance.basic_auth = nil
+    end
+
+    describe "initial ids" do
+      it "should use the subscriptions url to get the initial ids" do
+        Weeter.configure do |config|
+          config.oauth = {}
+          config.subscriptions_url = "http://www.site.com/ids"
+        end
+
+        EM::HttpRequest.should_receive(:new).with("http://www.site.com/ids").and_return(@http_request)
+        Weeter::TweetConsumer.stub!(:new).and_return(@consumer)
+
+        runner = Weeter::Runner.new(Weeter::Configuration.instance)
+        runner.start
+      end
+      
+      it "should parse JSON in the response to get the IDS" do
+        Weeter.configure do |config|
+          config.oauth = {}
+          config.subscriptions_url = "http://www.site.com/ids"
+        end
+        Weeter::TweetConsumer.stub!(:new).and_return(@consumer)
+        @http = mock(:http, :response_header => mock(:header, :status => 200), :response => [{'twitter_user_id' => '44'}, {'twitter_user_id' => '33'}].to_json)
+        @http.should_receive(:callback).and_yield
+        @http_request = mock(:http_request, :get => @http)
+        EM::HttpRequest.stub!(:new => @http_request)
+        EM.stub!(:stop)
+        
+        runner = Weeter::Runner.new(Weeter::Configuration.instance)
+        @consumer.should_receive(:connect).with(["44","33"])
+        runner.start
+      end
     end
 
     describe 'when using oauth' do
