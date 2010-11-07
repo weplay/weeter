1da074b77e1b0490dd51d96356b87e54f71cf576
Noah Davis <noahd1@yahoo.com>
2010-11-07 18:51:58 -0500

    Fix specs

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -5,12 +5,54 @@ describe Weeter::Configuration do
     Weeter::Configuration.instance.listening_port.should == 7337
   end
 
-  %w{twitter_oauth twitter_basic_auth publish_url delete_url subscriptions_url listening_port oauth}.each do |setting|
-    it "should accept setting for #{setting}" do
-      Weeter.configure do |conf|
-        conf.send("#{setting}=", "testvalue")
+  describe "ClientAppConfiguration" do
+    %w{delete_url subscriptions_url oauth}.each do |setting|
+      it "should accept setting for #{setting}" do
+        Weeter.configure do |conf|
+          conf.client_app do |app|
+            app.send("#{setting}=", "testvalue")
+          end
+        end
+        Weeter::ClientAppConfiguration.instance.send(setting).should == "testvalue"
       end
-      Weeter::Configuration.instance.send(setting).should == "testvalue"
     end
   end
+
+  describe "TwitterConfiguration" do
+    %w{basic_auth oauth}.each do |setting|
+      it "should accept setting for #{setting}" do
+        Weeter.configure do |conf|
+          conf.twitter do |app|
+            app.send("#{setting}=", "testvalue")
+          end
+        end
+        Weeter::TwitterConfiguration.instance.send(setting).should == "testvalue"
+      end
+    end
+    
+    describe "auth_options" do
+      
+      before do
+        Weeter::TwitterConfiguration.instance.oauth = nil
+        Weeter::TwitterConfiguration.instance.basic_auth = nil
+      end
+      
+      it "should return the oauth settings with a oauth credentials" do
+        Weeter::TwitterConfiguration.instance.oauth = {:consumer_key => 'consumer_key', :consumer_secret => 'consumer_secret', :access_key => 'acces_key', :access_secret => 'access_secret'}
+        Weeter::TwitterConfiguration.instance.auth_options.should == {:oauth => {:consumer_key => 'consumer_key', :consumer_secret => 'consumer_secret', :access_key => 'acces_key', :access_secret => 'access_secret'}}
+      end
+      
+      it "should return the basic auth settings separated by a colon" do
+        Weeter::TwitterConfiguration.instance.basic_auth = {:username => "bob", :password => "s3cr3t"}
+        Weeter::TwitterConfiguration.instance.auth_options.should == {:auth => "bob:s3cr3t"}
+      end
+    
+      it "should prefer oauth over basic auth" do
+        Weeter::TwitterConfiguration.instance.basic_auth = {:username => "bob", :password => "s3cr3t"}
+        Weeter::TwitterConfiguration.instance.oauth = {:consumer_key => 'consumer_key', :consumer_secret => 'consumer_secret', :access_key => 'acces_key', :access_secret => 'access_secret'}
+        Weeter::TwitterConfiguration.instance.auth_options.should == {:oauth => {:consumer_key => 'consumer_key', :consumer_secret => 'consumer_secret', :access_key => 'acces_key', :access_secret => 'access_secret'}}
+      end
+    end
+  end
+
 end
--- spec/tweet_consumer_spec.rb
+++ spec/tweet_consumer_spec.rb
@@ -1,38 +1,33 @@
 require 'spec_helper'
 
 describe Weeter::TweetConsumer do
-  before(:each) do
-    @ids = [1,2,3]
-    @consumer = Weeter::TweetConsumer.new(:twitter_auth_options => {:auth => 'joe:schmoe'}, :publish_url => 'http://mysite.co', :delete_url => 'http://mysite.co/delete')
-    @mock_stream = mock('JSONStream', :each_item => nil, :on_error => nil, :on_max_reconnects => nil)
-    Twitter::JSONStream.stub!(:connect).and_return(@mock_stream)
-    @mock_request = mock('HttpRequest', :post => nil)
-    EM::HttpRequest.stub!(:new).and_return(@mock_request)
-    @tweet_values = {'text' => "Hey", 'id_str' => "123", 'user' => {'id_str' => "1"}}
-    @mock_stream.stub!(:each_item).and_yield(@tweet_values.to_json)
-  end
+
 
   describe "auth" do
-    it 'should use oauth when oauth is the provided authentication' do
-      oauth_params = {:consumer_key => 'consumer_key', :consumer_secret => 'consumer_secret',
-                     :access_key => 'acces_key', :access_secret => 'access_secret'}
+    it 'should use connect to JSON stream with auth options for the configuration' do
+      @mock_stream = mock('JSONStream', :each_item => nil, :on_error => nil, :on_max_reconnects => nil)
+      Twitter::JSONStream.stub!(:connect).and_return(@mock_stream)
 
-      consumer = Weeter::TweetConsumer.new(:twitter_auth_options => {:oauth => oauth_params})
-
-      Twitter::JSONStream.should_receive(:connect).with(hash_including(:oauth => oauth_params))
-
-      consumer.connect([1,2])
-    end
-
-    it 'should use basic_auth when basic_auth is the provided authentication' do
-      consumer = Weeter::TweetConsumer.new(:twitter_auth_options => {:auth => 'username:password'})
-      Twitter::JSONStream.should_receive(:connect).with(hash_including(:auth => "username:password"))
+      Weeter::TwitterConfiguration.instance.stub!(:auth_options).and_return(:foo => :bar)
+      consumer = Weeter::TweetConsumer.new(Weeter::TwitterConfiguration.instance, mock('ClientAppProxy'))
+      Twitter::JSONStream.should_receive(:connect).with(hash_including(:foo => :bar))
       consumer.connect([1,2])
     end
   end
 
   describe "connecting to twitter" do
 
+    before(:each) do
+      @ids = [1,2,3]
+      Weeter::TwitterConfiguration.instance.stub!(:auth_options).and_return(:foo => :bar)
+      @tweet_values = {'text' => "Hey", 'id_str' => "123", 'user' => {'id_str' => "1"}}
+      @mock_stream = mock('JSONStream', :on_error => nil, :on_max_reconnects => nil)
+      @mock_stream.stub!(:each_item).and_yield(@tweet_values.to_json)
+      Twitter::JSONStream.stub!(:connect).and_return(@mock_stream)
+      @client_proxy = mock('ClientAppProxy', :publish_tweet => nil)
+      @consumer = Weeter::TweetConsumer.new(Weeter::TwitterConfiguration.instance, @client_proxy)
+    end
+    
     after(:each) do
       @consumer.connect(@ids)
     end
@@ -42,44 +37,28 @@ describe Weeter::TweetConsumer do
       Weeter::TweetItem.should_receive(:new).with({'text' => "Hey", 'id_str' => "123", 'user' => {'id_str' => "1"}}).and_return(tweet_item)
     end
 
-    describe "publishable" do
-      it "should connect to a Twitter JSON stream" do
-        Twitter::JSONStream.should_receive(:connect).
-          with(:auth => "joe:schmoe", :params => {:follow => @ids}, :method => 'POST')
-      end
-
-      it "should publish new tweet if publishable" do
-        tweet_item = Weeter::TweetItem.new(@tweet_values)
-        tweet_item.stub!(:publishable?).and_return(true)
-        Weeter::TweetItem.stub!(:new).and_return(tweet_item)
-        EM::HttpRequest.should_receive(:new).with('http://mysite.co').and_return(@mock_request)
-        @mock_request.should_receive(:post).with(:body => {:id => '123', :text => 'Hey', :twitter_user_id => '1'})
-      end
+    it "should connect to a Twitter JSON stream" do
+      Twitter::JSONStream.should_receive(:connect).
+        with(:foo => :bar, :params => {:follow => @ids}, :method => 'POST')
     end
-
-    describe "not publishable" do
-      it "should not publish non publishable tweets" do
-        tweet_item = Weeter::TweetItem.new(@tweet_values)
-        tweet_item.stub!(:publishable?).and_return(false)
-        Weeter::TweetItem.stub!(:new).and_return(tweet_item)
-        EM::HttpRequest.should_not_receive(:new)
-      end
+  
+    it "should publish new tweet if publishable" do
+      mock_tweet = mock('tweet', :deletion? => false, :publishable? => true)
+      tweet_item = Weeter::TweetItem.stub!(:new).and_return mock_tweet
+      @client_proxy.should_receive(:publish_tweet).with(mock_tweet)
     end
-
-    describe "deletion" do
-      it "should respond to delete requests" do
-        delete_values = {"delete"=>{"status"=>{"id"=>234, "user_id"=>34555}}}
-        @mock_stream.stub!(:each_item).and_yield(delete_values.to_json)
-
-        tweet_item = Weeter::TweetItem.new(delete_values)
-        tweet_item.stub!(:deletion?).and_return(true)
-        Weeter::TweetItem.stub!(:new).and_return(tweet_item)
-
-        EM::HttpRequest.should_receive(:new).with('http://mysite.co/delete').and_return(@mock_request)
-        @mock_request.should_receive(:delete).with(:body => {:id => '234', :twitter_user_id => '34555'})
-      end
+    
+    it "should not publish unpublishable tweets" do
+      mock_tweet = mock('tweet', :deletion? => false, :publishable? => false, :[] => '')
+      tweet_item = Weeter::TweetItem.stub!(:new).and_return mock_tweet
+      @client_proxy.should_not_receive(:publish_tweet).with(mock_tweet)
     end
-
+    
+    it "should delete deletion tweets" do
+      mock_tweet = mock('tweet', :deletion? => true, :publishable? => false)
+      tweet_item = Weeter::TweetItem.stub!(:new).and_return mock_tweet
+      @client_proxy.should_receive(:delete_tweet).with(mock_tweet)
+    end
   end
 
 end
